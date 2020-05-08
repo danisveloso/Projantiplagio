14c05af82cb72dab116da569a2aa3553e911355e
danielle <danisvcosta@gmail.com>
2020-05-07 21:11:15 -0300

    inserindo bootstrap e css template

--- systemAntiplagio/urls.py
+++ systemAntiplagio/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     url(r'^accounts/login/$', auth_views.LoginView.as_view(), name='login'),
-    url(r'^accounts/logout/$', auth_views.LogoutView.as_view(next_page="/"), name='logout'),
+    url(r'^accounts/logout/$', auth_views.LogoutView.as_view(next_page="login"), name='logout'),
    
 
     path('quiz/', include('quiz.urls')),
